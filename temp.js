e19627f9d6162da717c9fdfee9b2ef74672dc85f
albyh <albyheredia@gmail.com>
2016-03-25 14:14:18 -0700

    first solution of missing letters algorithm

--- temp.js
+++ temp.js
@@ -5,12 +5,23 @@ function doStuff( msg ){
 /************************************************************************************/
 
 function fearNotLetter(str) {
-  return str;
+  //if charCode of next character != i+1 return i+1
+  
+  var i = 0, j = str.length, fnl ; // fnl = FearNotLetter and is returned undefined if none
+
+
+  while ( (str.charCodeAt(i+1) - str.charCodeAt(i) === 1) || i === j ){
+    i++;
+  } 
+
+  fnl = (i+1) === j ? undefined : String.fromCharCode( str.charCodeAt(i)+1 ) ;
+
+  return fnl;
 }
 
 /********** Change the inputs below to test ************/
-console.log( "Solution output ----> " + fearNotLetter("abce") + " <----");
-fearNotLetter("abce");
+console.log( "Solution output ----> " + fearNotLetter("bcd") + " <----");
+fearNotLetter("bcd"); 
 
 
 /************************************************************************************/
